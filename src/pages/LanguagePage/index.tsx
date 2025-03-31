4ef272dd86ccc1bea55bae04db90aeec9e4ce1cb
Grigoryy <ctrhtnysq.afqk@gmail.com>
2025-03-31 13:21:44 +0700

    Разделяю код на компоненты. Рефакторю Пока убрад все обработчики событий для простоты

--- src/pages/LanguagePage/index.tsx
+++ src/pages/LanguagePage/index.tsx
@@ -1,7 +1,16 @@
 import { LanguageList } from "../../widgets/Language";
+import { SerachForm } from "../../widgets/Language";
+import { Container, Flex } from "@mantine/core";
+import { GET_LANGUAGES } from "../../shared/lib/api/stubs/Language";
 
 export const LanguagePage = () => {
     return (
-        <LanguageList />
+        <Container style={{ width: '100%' }}>
+            <Flex direction="column">
+                <SerachForm />
+                <LanguageList content={GET_LANGUAGES.content} pagination={GET_LANGUAGES.pagination}/>
+            </Flex>
+        </Container>
+
     );
 };
--- src/widgets/Language/index.tsx
+++ src/widgets/Language/index.tsx
@@ -3,137 +3,107 @@ import { Language } from "../../shared/lib/api/entities/Language";
 
 import { Button, Container, Flex, Title, Card, Text, TextInput, Grid, Modal } from "@mantine/core";
 import { useState } from "react";
-import { GET_LANGUAGES } from "../../shared/lib/api/stubs/Language";
+import { LanguagePage } from "../../shared/lib/api/entities";
 
 import EditIcon from './edit';
 import TrashIcon from './trash';
 
-interface LanguageFormProps {
-    language: Language | null;
-    onSubmit: (language: Language) => void;
+const inputFields: InputField[] = [
+    { name: 'languageName', placeholder: 'Введите название языка' }
+];
+
+// Поле для ввода текста (название и что отобразить в пустом поле для ввода)
+interface InputField {
+    name: string;
+    placeholder: string;
 }
-export const LanguageForm = ({ language, onSubmit }: LanguageFormProps) => {
-    const form = useForm<Language>({
-        initialValues: {
-            id: language ? language.id : `language_id_${Math.random()}`,
-            name: language ? language.name : ''
-        }
-    });
-    const handleSubmit = (event: React.FormEvent) => {
-        event.preventDefault();
-        onSubmit(form.values);
-    };
-    return (
-        <form onSubmit={handleSubmit}>
-            <Flex align="center" gap="sm">
-                <TextInput required {...form.getInputProps('name')} />
-                <Flex mr="md">
-                    <Button type='submit' color="gray" style={{ width: '48px', height: '48px', padding: 0 }}>
+
+// Интерфейс для вывода модального окна с определёнными полями
+interface ModalProps {
+    title: string;
+    inputFields: InputField[];
+    isOpen: boolean;
+    onClose: () => void;
+}
+
+export function SerachForm(){
+    const [isModalOpen, setModalOpen] = useState(false);
+    return(
+        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '20px' }}>
+        <TextInput
+            placeholder="Поиск языка..."
+            //onChange={(event) => setSearchTerm(event.currentTarget.value)}
+        />
+        <Button onClick={() => setModalOpen(true)}  style={{ marginLeft: '10px' }}>
+            Создать язык
+        </Button>
+        <ModalForm title="Создание нового языка" inputFields={inputFields} isOpen={isModalOpen} onClose={() => setModalOpen(false)}/>
+    </div>
+    )
+}
+
+function LanguageCard({id, name}: Language){
+    return(
+        <Card key={id} shadow="sm" padding="lg" style={{ width: '100%', maxWidth: '300px', height: '64px' }}>
+            <Flex justify="space-between" align="center" style={{ height: '100%' }}>
+                <Text>{name}</Text>
+                <Flex gap="md">
+                    <Button color="gray" /*onClick={() => handleEdit(lang)}*/ style={{ width: '48px', height: '48px', padding: 0, /*display: isEditing[lang.id] ? 'none' : 'inline'*/ }}>
                         <EditIcon />
                     </Button>
-                </Flex>
-            </Flex>
-        </form>
-    );
-};
-export const LanguageList = () => {
-    const [languages, setLanguages] = useState(GET_LANGUAGES.content);
-    const [selectedLanguage, setSelectedLanguage] = useState<Language | null>(null);
-    const [isEditing, setIsEditing] = useState<{ [key: string]: boolean }>({});
-    const [searchTerm, setSearchTerm] = useState('');
-
-    const [modalOpen, setModalOpen] = useState(false);
-    const [newLanguageName, setNewLanguageName] = useState('');
-    const handleDelete = (id: string) => {
-        setLanguages(languages.filter(lang => lang.id !== id));
-    };
-    const handleEdit = (language: Language) => {
-        const newEditingState = {
-            ...isEditing,
-            [language.id]: !isEditing[language.id]
-        };
-        setIsEditing(newEditingState);
-        if (selectedLanguage && selectedLanguage.id === language.id) {
-            handleCreateOrUpdate(language);
-            setSelectedLanguage(null);
-        } else {
-            setSelectedLanguage(language);
-        }
-    };
-    const handleCreateOrUpdate = (language: Language) => {
-        if (selectedLanguage) {
-            setLanguages(languages.map(lang => lang.id === language.id ? language : lang));
-        } else {
-            setLanguages([...languages, language]);
-        }
-        setSelectedLanguage(null);
-        setIsEditing({});
-    };
-    const handleCreateNewLanguage = () => {
-        if (newLanguageName.trim()) {
-            const newLanguage: Language = {
-                id: `language_id_${Math.random()}`,
-                name: newLanguageName.trim()
-            };
-            setLanguages([...languages, newLanguage]);
-            setNewLanguageName('');
-            setModalOpen(false);
-        }
-    };
-    const filteredLanguages = languages.filter(lang => lang.name.toLowerCase().includes(searchTerm.toLowerCase()));
-    return (
-        <Container style={{ width: '100%' }}>
-            <Flex direction="column">
-                <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '20px' }}>
-                    <TextInput
-                        placeholder="Поиск языка..."
-                        value={searchTerm}
-                        onChange={(event) => setSearchTerm(event.currentTarget.value)}
-                    />
-                    <Button onClick={() => setModalOpen(true)}  style={{ marginLeft: '10px' }}>
-                        Создать язык
+                    <Button color="red" /*onClick={() => handleDelete(lang.id)}*/ style={{ width: '48px', height: '48px', padding: 0 }}>
+                        <TrashIcon />
                     </Button>
-                </div>
-                <Flex wrap="wrap" gap="md" mt="lg" style={{ width: '100%' }}>
-                    {filteredLanguages.map(lang => (
-                        <Card key={lang.id} shadow="sm" padding="lg" style={{ width: '100%', maxWidth: '300px', height: '64px' }}>
-                            <Flex justify="space-between" align="center" style={{ height: '100%' }}>
-                                {selectedLanguage && selectedLanguage.id === lang.id ? (
-                                    <LanguageForm language={lang} onSubmit={handleCreateOrUpdate} />
-                                ) : (
-                                    <Text>{lang.name}</Text>
-                                )}
-                                <Flex gap="md">
-                                    <Button color="gray" onClick={() => handleEdit(lang)} style={{ width: '48px', height: '48px', padding: 0, display: isEditing[lang.id] ? 'none' : 'inline' }}>
-                                        <EditIcon />
-                                    </Button>
-                                    <Button color="red" onClick={() => handleDelete(lang.id)} style={{ width: '48px', height: '48px', padding: 0 }}>
-                                        <TrashIcon />
-                                    </Button>
-                                </Flex>
-                            </Flex>
-                        </Card>
-                    ))}
                 </Flex>
             </Flex>
+        </Card>
+    )
+}
 
-            <Modal
-                opened={modalOpen}
-                onClose={() => setModalOpen(false)}
-                title="Создание нового языка"
-            >
-                <TextInput
-                    placeholder="Введите название языка"
-                    value={newLanguageName}
-                    onChange={(event) => setNewLanguageName(event.currentTarget.value)}
-                />
-                <Button
-                    onClick={handleCreateNewLanguage}
-                    style={{ marginTop: '10px' }}
-                >
-                    Создать
-                </Button>
-            </Modal>
-        </Container>
+export function LanguageList({ content }: LanguagePage) {
+    return (
+        <Flex wrap="wrap" gap="md" mt="lg" style={{ width: '100%' }}>
+                {content.map(card => (
+                <LanguageCard key={card.id} id={card.id} name={card.name} />
+            ))}
+        </Flex>
     );
 };
+
+function ModalForm({ title,  inputFields, isOpen, onClose} : ModalProps){
+    const [formValues, setFormValues] = useState<Record<string, string>>({});
+
+    const handleChange = (field: string, value: string) => {
+        setFormValues({
+            ...formValues,
+            [field]: value
+        });
+    };
+
+    const createNewObject = () => {
+        // Логика для создания нового объекта (пока язык или стек)
+        console.log(formValues);
+        onClose();
+    }
+
+    if (!isOpen) return null; 
+    return(
+        <Modal title={title} opened={isOpen} onClose={onClose}>
+            {inputFields.map((field, index) => (
+                <TextInput
+                    key={index}
+                    placeholder={field.placeholder}
+                    value={formValues[field.name] || ''}
+                    onChange={(event) => handleChange(field.name, event.currentTarget.value)}
+                />
+            ))}
+            <Button
+                onClick={createNewObject}
+                style={{ marginTop: '10px' }}
+            >
+                Создать
+            </Button>
+        </Modal >
+    )
+};
+
