81e1c3d713ba9df185f08fa10af7decc642a66c6
Grigoryy <ctrhtnysq.afqk@gmail.com>
2025-04-04 22:34:45 +0700

    Поправил комментарии

--- src/entity/CommentCard/index.tsx
+++ src/entity/CommentCard/index.tsx
@@ -1,29 +1,12 @@
-import { Card, Flex} from "@mantine/core";
+import { Card, Flex, Avatar} from "@mantine/core";
 import { InterviewsComment } from "shared/lib";
 import { UserIcon } from "./UserIcon/UserIcon";
 
 export function CommentCard({ id, content, createdAt, modifiedAt, author }: InterviewsComment) {
-
-    function getColorById(id: string) {
-        const colors = [
-            '#B22222', '#4CAF50', '#3F51B5', '#D3D300', '#D5006D', '#009688', '#FF8C00', '#6A5ACD', '#FF4500', '#3CB371'
-        ];
-        const index = Math.abs(hashCode(id)) % colors.length;
-        return colors[index];
-    }
-    
-    function hashCode(str: string) {
-        let hash = 0;
-        for (let i = 0; i < str.length; i++) {
-            hash = str.charCodeAt(i) + ((hash << 5) - hash);
-        }
-        return hash;
-    }
-
     return (
         <Card className="comment" id={id} style={{width: '100%', padding:'0.25vh', minHeight: '48px'}}>
             <Flex align="flex-start">
-                <UserIcon color={getColorById(id)}/>
+                {<Avatar color={getColorById(id)}>{/* author.avatar != null ? author.avatar :*/ author.firstName[0].toUpperCase() + author.lastName[0].toUpperCase()}</Avatar> }
                 <div style={{ flex: 1, marginLeft: '10px'}}>
                     <Flex direction="column">
                         <Flex align="center">
@@ -38,4 +21,20 @@ export function CommentCard({ id, content, createdAt, modifiedAt, author }: Inte
             </Flex>
         </Card>
     )
+}
+
+function getColorById(id: string) {
+    const colors = [
+        '#B22222', '#4CAF50', '#3F51B5', '#D3D300', '#D5006D', '#009688', '#FF8C00', '#6A5ACD', '#FF4500', '#3CB371'
+    ];
+    const index = Math.abs(hashCode(id)) % colors.length;
+    return colors[index];
+}
+
+function hashCode(str: string) {
+    let hash = 0;
+    for (let i = 0; i < str.length; i++) {
+        hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    return hash;
 }
