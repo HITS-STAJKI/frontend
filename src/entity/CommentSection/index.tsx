ca25b97ac1727c87dffab556f49f1df0d437b4eb
Dmitriy Slabukhin <dmitryslabukhin@gmail.com>
2025-04-05 09:37:42 +0700

    перенес hashcode в lib

--- src/entity/CommentSection/index.tsx
+++ src/entity/CommentSection/index.tsx
@@ -1,16 +1,8 @@
 import { Avatar, Container, Flex, Group, MantineColor, Space, Stack, Text, Title } from "@mantine/core"
 import { CreateCommentForm } from "features/CreateCommentForm"
-import { dateFormatter, InterviewsComment } from "shared/lib"
+import { dateFormatter, hashCode, InterviewsComment } from "shared/lib"
 
 const AvatarColor: Array<MantineColor> = ['dark', 'gray', 'red', 'pink', 'grape', 'violet', 'indigo', 'blue', 'cyan', 'green', 'lime', 'yellow', 'orange', 'teal']
-
-const hashCode = (str: string) => {
-    let hash = 0;
-    for (let i = 0; i < str.length; i++) {
-        hash = str.charCodeAt(i) + ((hash << 5) - hash);
-    }
-    return hash;
-}
 
 type CommentSectionProps = {
     comments: Array<InterviewsComment>
