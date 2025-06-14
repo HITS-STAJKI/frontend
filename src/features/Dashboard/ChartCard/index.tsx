a01b8d9a252e2d6731202ab8e5b3430d88f94468
Dmitriy Slabukhin <dmitryslabukhin@gmail.com>
2025-06-14 18:54:56 +0700

    Изменил диаграмку

--- src/features/Dashboard/ChartCard/index.tsx
+++ src/features/Dashboard/ChartCard/index.tsx
@@ -1,26 +1,11 @@
-import { Chart } from "react-charts"
-import { useChart } from "./hook"
 import { Card } from "@mantine/core"
+import { Bar } from '@nivo/bar'
 
-type Props = {
-    x: string
-    date: Date,
-    name: string
+type ChartCardProps = {
+    filter?: 
 }
 
 export const ChartCard = () => {
-
-    const { options } = useChart<Props>(['x', 'date'], 'bar', [
-        {
-            date: new Date(2025, 4, 18),
-            x: 'Поток 9722',
-            name: '123'
-        },
-        {
-            date: new Date(2025, 4, 19),
-            x: 'Поток 9723',
-            name: '321'
-        },])
     return (
         <Card style={{ width: '30%', height: 'fit-content' }} shadow='sm' withBorder p={'lg'}>
             <Card.Section>
@@ -28,7 +13,7 @@ export const ChartCard = () => {
             </Card.Section>
             <Card.Section>
                 <div style={{ width: '100%', height: '300px' }}>
-                    <Chart options={options} />
+                    <Bar data={ } />
                 </div>
             </Card.Section>
         </Card>
