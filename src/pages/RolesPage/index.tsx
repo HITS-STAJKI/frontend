97f306dd6d4f444796f8a136d0069a072fd682c7
Kseniya Yakovleva <ksushaykovleva2004@gmail.com>
2025-06-22 12:39:23 +0700

    Добавлена фильтрация в пользователях

--- src/pages/RolesPage/index.tsx
+++ src/pages/RolesPage/index.tsx
@@ -1,4 +1,6 @@
-import { Container, Flex } from "@mantine/core";
+import { Container, Flex, Text } from "@mantine/core";
+import { useState } from "react";
+import { UserRole } from "services/api/api-client.types";
 import { useGetPartnersQuery } from "services/api/api-client/CompanyPartnersQuery";
 import { useGetGroupsQuery } from "services/api/api-client/GroupQuery";
 import { useGetUserListQuery } from "services/api/api-client/UserQuery";
@@ -6,18 +8,40 @@ import { Pagination } from "shared/ui";
 import { UsersList, SearchRolesForm } from "widgets/RolesForm"
 
 export const RolesPage = () => {
-    const { data, isLoading } = useGetUserListQuery()
-    const { data: groups, isLoading: isLoadingGroups } = useGetGroupsQuery()
-    const { data: partners, isLoading: isLoadingPartners } = useGetPartnersQuery()
-    if (isLoading || isLoadingGroups || isLoadingPartners) {
+    const [searchParams, setSearchParams] = useState<{
+        name?: string;
+        role?: UserRole;
+    }>({});
+
+    const { data, isLoading } = useGetUserListQuery({
+        fullName: searchParams.name,
+        userRole: searchParams.role,
+    });
+    // const { data: groups, isLoading: isLoadingGroups } = useGetGroupsQuery()
+    // const { data: partners, isLoading: isLoadingPartners } = useGetPartnersQuery()
+
+    const handleSearch = (values: typeof searchParams) => {
+        setSearchParams(values);
+    };
+
+    if (isLoading) {
         return 'Загрузка'
     }
     return (
         <Container fluid>
-            <Flex direction="column" style={{ width: '75%', margin: '0 auto' }}>
-                <SearchRolesForm groupContent={groups?.items!} companyContent={partners?.items!} />
-                <UsersList items={data?.items!} pagination={data?.pagination!} />
-                <Pagination pagination={data?.pagination} />
+            <Flex direction="column" style={{ width: '75%', margin: '0 auto',  minWidth: '900px' }}>
+                <SearchRolesForm onSearch={handleSearch} />
+                {data?.items?.length === 0 ? (
+                    <Text>Пользователи не найдены</Text>
+                ) : (
+                    <>
+                        <UsersList 
+                            items={data?.items!} 
+                            pagination={data?.pagination!} 
+                        />
+                        <Pagination pagination={data?.pagination} />
+                    </>
+                )}
             </Flex>
         </Container>
     );
