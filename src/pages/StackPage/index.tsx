20be470fac26e8e9b5692798adb708f9afd2120c
Dmitriy Slabukhin <dmitryslabukhin@gmail.com>
2025-06-26 13:55:57 +0700

    fix

--- src/pages/StackPage/index.tsx
+++ src/pages/StackPage/index.tsx
@@ -9,32 +9,6 @@ export const StackPage = () => {
     const [searchQuery, setSearchQuery] = useState('');
     const { data, isLoading, isError, error } = useGetStackListQuery(searchQuery);
 
-    if (isLoading) {
-        return (
-            <Center style={{ position: 'fixed', top: 0, left: 0, width: '100vw', height: '100vh', pointerEvents: 'none', zIndex: 9999 }} >
-                <Loader size="lg" />
-            </Center>
-        );
-    }
-
-    if (isError) {
-        return (
-            <Card mt="md" p="md" style={{ backgroundColor: '#ffe6e6', borderRadius: 6, width: '100%' }}>
-                <Text color="red" size="sm" style={{ textAlign: 'center' }}>
-                    Ошибка: {getErrorMessage(error)}
-                </Text>
-            </Card>
-        );
-    }
-
-    if (!data) {
-        return (
-            <Center style={{ position: 'fixed', top: 0, left: 0, width: '100vw', height: '100vh', pointerEvents: 'none', zIndex: 9999 }} >
-                <Text>Нет данных</Text>
-            </Center>
-        );
-    }
-
     return (
         <Container style={{ width: '100%' }}>
             <Flex direction="column">
